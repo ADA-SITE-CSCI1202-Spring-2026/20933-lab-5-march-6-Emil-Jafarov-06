d886baee3bfd5e3150b2367b642f65136555af88
Emil Jafarov <emiljafarov3841@gmail.com>
2026-03-06 14:02:33 +0400

    Tester classes improved

--- Exercise_2/RectangleTester.java
+++ Exercise_2/RectangleTester.java
@@ -2,13 +2,13 @@ package Exercise_2;
 
 public class RectangleTester {
 
-    public static void main() {
+    public static void main(String[] args) {
         Rectangle r1 = new Rectangle(5, 10);
         Rectangle r2 = new Rectangle(15, 10);
         Rectangle r3 = new Rectangle(5, 10);
 
-        System.out.println(r1.equals(r2));
-        System.out.println(r1.equals(r3));
+        System.out.println("Objects are identical: " + r1.equals(r2));
+        System.out.println("Objects are identical: " + r1.equals(r3));
 
     }
 
